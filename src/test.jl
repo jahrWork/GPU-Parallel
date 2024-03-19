41ce42ff3b2803ac981b695372576c89c8b6583d
Juan Hernandez <jahramos@gmail.com>
2024-03-19 19:37:00 +0100

    test

--- src/test.jl
+++ src/test.jl
@@ -9,20 +9,17 @@ using CPUTime
 using Plots
 
 function matrix_initialization(N)
-
   
     A = rand(Float32, N, N )
     B = rand(Float32, N, N )
 
     return A, B 
-
 end 
 
 
 function matrix_multiplication(A,B)
 
      return A * B  
-  
 end
 
 
@@ -31,7 +28,7 @@ function time_for_different_N(N, N_cores)
   Time = zeros( length(N) )
   Theoretical_time = 4e9/(4e9 * 512/32 * 4 * N_cores)
 
-  for (i,n) in enumerate(N)  # variables inside loop are local scope 
+  for (i,n) in enumerate(N)  # variables inside loop have local scope 
  
    A,B = matrix_initialization(n)
 
@@ -64,13 +61,14 @@ N = Vector([10:10:2000; 2000:100:6000])
 #using LinearAlgebra, BLIS
 using LinearAlgebra, MKL
 
+begin
 N_cores = 4 
 BLAS.set_num_threads(2*N_cores)
 println(" threads = ", BLAS.get_num_threads() )
 Time, Theoretical_time = time_for_different_N(N, N_cores)
 display( plot(N, Time, ylims=(0., 0.05) ) )
 display( plot!(N, Theoretical_time*ones( length(N) ) ) )
-
+end
 
 
 # BLAS.set_num_threads(Sys.CPU_THREADS)
@@ -78,14 +76,6 @@ display( plot!(N, Theoretical_time*ones( length(N) ) ) )
 # Time = time_for_different_N(N)
 # plot(N, Time)    
 
-
-# begin 
-# BLAS.set_num_threads(4)
-# println(" threads = ", BLAS.get_num_threads() )
-# Time, Theoretical_time = time_for_different_N(N)
-# plot(N, Time, ylims=(0., 0.05) )
-# plot!(N, Theoretical_time*ones( length(N) ) ) 
-# end
 
 
 # ENV["JULIA_NUM_THREADS"] = "4"
