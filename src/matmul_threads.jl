8e951b7b00f2a77a2d5d35c1a98123e03b3da046
RayleighLord <javier.gonzalez.monge@gmail.com>
2024-05-01 20:38:32 +0200

    Update

--- src/matmul_threads.jl
+++ src/matmul_threads.jl
@@ -36,10 +36,28 @@ function serial_multiply(A, B, N_threads)
     end
 end
 
-# N_threads = Threads.nthreads()
-N_threads = 8
+N_threads = Threads.nthreads()
+# N_threads = 8
 
 A, B = matrix_initialization(100)
 
+N = Vector([10:50:2500; 2500:250:5000])
+
 @time parallel_multiply(A, B, N_threads)
 @time serial_multiply(A, B, N_threads)
+
+time_parallel = zeros(length(N))
+time_serial = zeros(length(N))
+
+for (i, n) in enumerate(N)
+    A, B = matrix_initialization(n)
+    GC.gc()
+    time_parallel[i] = @elapsed parallel_multiply(A, B, N_threads)
+    time_serial[i] = @elapsed serial_multiply(A, B, N_threads)
+    @show n, time_parallel[i], time_serial[i]
+end
+
+display(plot(N, time_parallel, title = "Time", minorgrid = true))
+display(plot!(N, time_serial, minorgrid = true))
+
+xlabel!("N")
