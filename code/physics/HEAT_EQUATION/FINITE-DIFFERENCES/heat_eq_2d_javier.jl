1f7d193375fae9e528590af962b3aab1fb4a35be
romanrusset <99611515+romanrusset@users.noreply.github.com>
2024-10-08 17:46:11 +0200

    HEAT EQUATION FOLDER
    The folder includes two subfolders: finite differences and global interpolation. Inside each of these subfolders, there are two codes: one optimized (which uses matrix multiplication) and one non-optimized (which uses matrix-vector multiplication). Additionally, in the finite differences folder, there is a code written by Javier. 
    For all the codes, there are several commented lines that would make the code solve the advection-diffusion equation.

---
+++ code/physics/HEAT_EQUATION/FINITE-DIFFERENCES/heat_eq_2d_javier.jl
@@ -0,0 +1,78 @@
+using Pkg
+Pkg.add("GLMakie")
+using LinearAlgebra, GLMakie
+
+
+function heatpropagate(u::AbstractArray{T,N}, timesteps::Integer, αΔtΔx⁻²::Real,
+    uprev::AbstractArray{T,N}) where {T<:Number,N}
+
+    αΔtΔx⁻² ≤ 0.5 / N || throw(ArgumentError("αΔtΔx⁻² = $αΔtΔx⁻² violates Fourier stability condition"))
+
+    unitvecs = ntuple(i -> CartesianIndex(ntuple(==(i), Val(N))), Val(N))
+
+    I = CartesianIndices(u)
+    Ifirst, Ilast = first(I), last(I)
+    I1 = oneunit(Ifirst)
+
+    for t = 1:timesteps
+
+        @inbounds @simd for i in Ifirst+I1:Ilast-I1
+
+            ∇²u = -2N * u[i]  #Finite differences here
+            for uvec in unitvecs
+                ∇²u += u[i+uvec] + u[i-uvec]
+            end
+
+            u[i] = uprev[i] + αΔtΔx⁻² * ∇²u
+
+        end
+
+        # Here Boundary conditions would be imposed (except if they are Dirichlet at boundary)
+
+        uprev = u
+    end
+
+    return u, uprev
+end
+
+begin
+    L = 1.0
+    nx = 25
+    α = 0.01
+    Fo = 1 / 4
+
+    dx = L / (nx - 1)
+    dt = 0.001
+    nt = 10
+
+    x = range(-L / 2, L / 2, length=nx)
+    y = range(-L / 2, L / 2, length=nx)
+    u = [exp(-((xi^2 + yi^2) / 0.5^2)) for yi in y, xi in x]
+    u[1, :] .= 0.0
+    u[end, :] .= 0.0
+    u[:, 1] .= 0.0
+    u[:, end] .= 0.0
+
+    uprev = copy(u)
+end
+
+begin
+    fig = Figure()
+    ax = Axis(fig[1, 1], xlabel="x", ylabel="y")
+    hm = heatmap!(ax, x, y, u, colormap=:viridis, colorrange=(0, 1))
+    Colorbar(fig[1, 2], hm, label="Temperature")
+
+    fig
+end
+
+
+begin
+    u, uprev = heatpropagate(u, nt, Fo, uprev)
+    heatmap!(ax, x, y, u, colormap=:viridis, colorrange=(0, 1))
+    fig
+    #print(u)
+end
+
+using BenchmarkTools
+@benchmark heatpropagate(u, nt, Fo, uprev)
+@time heatpropagate(u, nt, Fo, uprev)
---
+++ code/physics/HEAT_EQUATION/GLOBAL-INTERPOLANT/HEAT_EQ_GLOBAL-INT_MATxvec.jl
@@ -0,0 +1,144 @@
+using Pkg
+Pkg.add("LinearAlgebra")
+Pkg.add("SparseArrays")
+Pkg.add("Plots")
+Pkg.add("Kronecker")
+Pkg.add("BenchmarkTools")
+using LinearAlgebra, SparseArrays, Plots, Kronecker, BenchmarkTools
+
+function print_matrix(m)
+    for row in 1:size(m, 1)
+        println(join(m[row, :], " "))
+    end
+end
+
+
+nx = 25
+ny = 25
+Lx = 1.0
+Ly = 1.0
+alpha = 0.01
+v = [0.1, 0.1]
+dt = 0.001
+tfinal = 1
+nsteps = Int(tfinal / dt)
+dx = Lx / (nx - 1)
+dy = Ly / (ny - 1)
+
+function initialize_temperature(nx, ny, dx, dy)
+    T = zeros(nx, ny)
+    for i in 1:nx
+        for j in 1:ny
+            x = (i-1) * dx
+            y = (j-1) * dy
+            T[i,j] = exp(-(25*(x-0.5)^2 + 25*(y-0.5)^2))
+            if T[i,j] < 0
+                T[i,j] = 0
+            end
+        end
+    end
+    return T
+end
+
+function apply_boundary_conditions!(T, nx, ny, dx, dy)
+    # Mantener la temperatura fija en los bordes
+    T[1, :] .= exp(-(25*(-0.5)^2))
+    T[end, :] .= exp(-(25*(0.5)^2))
+    T[:, 1] .= exp(-(25*(-0.5)^2))
+    T[:, end] .= exp(-(25*(0.5)^2))
+
+    # Mantener la fuente térmica fija en el centro con la condición inicial
+    # for i in 1:nx
+    #     for j in 1:ny
+    #         x = (i-1) * dx
+    #         y = (j-1) * dy
+    #         distance = sqrt((x - 0.5)^2 + (y - 0.5)^2)
+    #         if distance <= 0.05
+    #             #T[i,j] = exp(-(25*(x-0.5)^2 + 25*(y-0.5)^2))
+    #             T[i,j] = 1
+    #         end
+    #     end
+    # end
+end
+
+nodes_x = range(0, stop=Lx, length=nx)
+nodes_y = range(0, stop=Ly, length=ny)
+
+function lagrange_basis(x_nodes, i, x)
+    l_i = 1.0
+    for j in 1:length(x_nodes)
+        if j != i
+            l_i *= (x - x_nodes[j]) / (x_nodes[i] - x_nodes[j])
+        end
+    end
+    return l_i
+end
+
+#!
+function lagrange_derivative(x_nodes, i, x)
+    dl_i = 0.0
+    for m in 1:length(x_nodes)
+        if m != i
+            term = 1.0 / (x_nodes[i] - x_nodes[m])
+            for j in 1:length(x_nodes)
+                if j != i && j != m
+                    term *= (x - x_nodes[j]) / (x_nodes[i] - x_nodes[j])
+                end
+            end
+            dl_i += term
+        end
+    end
+    return dl_i
+end
+
+function lagrange_derivative_matrix(x_nodes)
+    n = length(x_nodes)
+    D = zeros(n, n)
+    for i in 1:n
+        for j in 1:n
+            D[i, j] = lagrange_derivative(x_nodes, j, x_nodes[i])
+        end
+    end
+    return D
+end
+
+D_x = lagrange_derivative_matrix(nodes_x)
+D_y = lagrange_derivative_matrix(nodes_y)
+D2_x = D_x * D_x
+D2_y = D_y * D_y
+
+I_x = I(nx)
+I_y = I(ny)
+Laplacian = kron(D2_x, I_y) + kron(I_x, D2_y)
+Gradient = v[1] * kron(D_x, I_y) + v[2] * kron(I_x, D_y)
+
+T = initialize_temperature(nx, ny, dx, dy)
+T = vec(T)
+temperaturas = []
+
+function simulate_temperature!(T, temperaturas, nsteps, dt, alpha, Laplacian, Gradient, nx, ny, dx, dy)
+    @inbounds @simd for step in 1:nsteps
+        #For solving avd-dif equation
+        T = T .+ dt .* (alpha * (Laplacian * T))
+        #For solving avd-dif equation
+        #T = T .+ dt .* (alpha * (Laplacian * T) - (Gradient * T))
+        T = reshape(T, nx, ny)
+        apply_boundary_conditions!(T, nx, ny, dx, dy)
+        T = vec(T)
+        push!(temperaturas, copy(T))  # Guardamos el estado actual de la temperatura
+    end
+end
+
+@elapsed simulate_temperature!(T, temperaturas, nsteps, dt, alpha, Laplacian, Gradient, nx, ny, dx, dy)
+T = reshape(T, nx, ny)
+
+x = range(0, stop=Lx, length=nx)
+y = range(0, stop=Ly, length=ny)
+z_min, z_max = 0, 1.0
+intervalo_animacion = 30
+animation = @animate for i in 1:intervalo_animacion:length(temperaturas)
+    t = temperaturas[i]
+    contourf(x, y, reshape(t, nx, ny), c=:inferno, xlims=(0, Lx), ylims=(0, Ly), clim=(z_min, z_max), xlabel="", ylabel="", title="", clabels=false, colorbar=true)
+end
+
+gif(animation, "adveccion-difusion_finite-diff_MATxvec.gif", fps=10)
