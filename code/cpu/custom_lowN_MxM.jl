38fe297134c35b40c2e240ff375c5657c50bd027
Pedro Rodríguez Jiménez <pedro.rjimenez@alumnos.upm.es>
2024-10-20 11:38:36 +0200

    Reached 100% CPU usage with small matrix sizes, but getting same performance

--- code/cpu/custom_lowN_MxM.jl
+++ code/cpu/custom_lowN_MxM.jl
@@ -1,7 +1,6 @@
 using LinearAlgebra, BenchmarkTools, Plots, Base.Threads
 
-#No funciona
-
+# Función personalizada de multiplicación de matrices
 function custom_matmul(A::Array{Float32,2}, B::Array{Float32,2})
     N = size(A, 1)
     C = Array{Float32}(undef, N, N)
@@ -9,7 +8,7 @@ function custom_matmul(A::Array{Float32,2}, B::Array{Float32,2})
     @inbounds @threads for i = 1:N
         for j = 1:N
             sum = 0.0f0
-            for k = 1:N
+            @simd for k = 1:N
                 sum += A[i, k] * B[k, j]
             end
             C[i, j] = sum
@@ -18,12 +17,13 @@ function custom_matmul(A::Array{Float32,2}, B::Array{Float32,2})
     return C
 end
 
+# Función para medir el rendimiento de la multiplicación
 function time_custom_matmul(N_range)
     GFLOPS = zeros(length(N_range))
     for (i, N) in enumerate(N_range)
         A = rand(Float32, N, N)
         B = rand(Float32, N, N)
-        Nop = 2 * N^3
+        Nop = 2 * N^3  # Operaciones necesarias para la multiplicación
 
         # Definir el número de repeticiones
         num_reps = 10
@@ -41,6 +41,7 @@ function time_custom_matmul(N_range)
     return GFLOPS
 end
 
+# Función para graficar los resultados
 function plot_GFLOPS_custom()
     N_range = collect(100:10:200)
 
@@ -50,7 +51,7 @@ function plot_GFLOPS_custom()
     # Calcular GFLOPS teóricos
     CPU_frequency_GHz = 3.9
     CPU_frequency = CPU_frequency_GHz * 1e9
-    N_logical_cores = 12  # Usa todos los hilos de tu CPU
+    N_logical_cores = 12
     AVX_value = 8
 
     Theoretical_GFLOPS = (CPU_frequency * AVX_value * 2 * N_logical_cores) / 1e9
