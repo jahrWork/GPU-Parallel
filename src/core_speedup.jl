1cdfb747169c783d47c9d4f909c88016f258d3fa
Juan Hernandez <jahramos@gmail.com>
2024-10-28 09:52:18 +0100

    optimize  N low

--- src/core_speedup.jl
+++ src/core_speedup.jl
@@ -1,4 +1,7 @@
 using LinearAlgebra, MKL
+# import Pkg 
+# Pkg.add("BLAS")
+#using LinearAlgebra, BLAS
 
 
 function my_matrix_multiplication(A,B)
@@ -58,7 +61,7 @@ function my_efficient_matrix_multiplication2(A,B)
     return C 
 end 
 
-function my_efficient_matrix_multiplication3(A,B)
+function mult_Nt_parallel_1_thread(A,B)
   
     (N, M) = size(A)
     (M, L) = size(B) 
@@ -73,35 +76,82 @@ function my_efficient_matrix_multiplication3(A,B)
     return C 
 end 
 
+function mult_No_parallel_1_thread(A,B)
+  
+  (N, M) = size(A)
+  (M, L) = size(B) 
+  C = zeros(Float32,  (N, L) )
+ 
 
-function matrix_multiplication(A, B)
-    return A * B
+      BLAS.set_num_threads(1)
+      for k in 1:Nt 
+        C =  k * A * B 
+      end
+
+  return C 
+end 
+
+function  mult_Nt_times_parallel___(A, B)
+
+  (N, M) = size(A)
+  (M, L) = size(B) 
+  C = zeros(Float32,  (N, L) )
+ 
+
+  for k in 1:Nt 
+      C =  k * A * B 
+  end
+
+  return C 
 end
 
 
-N = 50
+function matrix_mult______________(A, B)
+
+
+    return A * B
+
+end
+
+function  matrix_mult_________alloc(A, B)
+
+  (N, M) = size(A)
+  (M, L) = size(B) 
+  C = zeros(Float32,  (N, L) )
+ 
+      C =   A * B 
+
+  return C 
+end
+
+
+N = 100
 A = rand(Float32, N, N)
 B = rand(Float32, N, N)
 Nt = 1000
 
 
-N_threads = [1, 2, 4, 8, 16, 32]
-N_threads = [ 4 ]
+#N_threads = [1, 2, 4, 8, 16, 32]
+#N_threads = [ 4 ]
 matmul_functions = ( 
     # (my_matrix_multiplication, 2*N^3), 
     # (my_efficient_matrix_multiplication, 2*N^3),
     # (my_efficient_matrix_multiplication2, 2*N^3),
-    (my_efficient_matrix_multiplication3, 2*N^3*Nt),
-    (matrix_multiplication, 2*N^3)  )
+    (mult_No_parallel_1_thread, 2*N^3*Nt), 
+
+    (mult_Nt_parallel_1_thread, 2*N^3*Nt),
+    (mult_Nt_times_parallel___, 2*N^3*Nt),
+
+    (matrix_mult_________alloc, 2*N^3),
+    (matrix_mult______________, 2*N^3)  )
 
 
 
 for (mult, Nop) in matmul_functions
 
-  println(" matmul function = ", mult)  
-  mult(A, B)
+   mult(A, B)
 
-  for threads in N_threads
+  #for threads in N_threads
 
   #  BLAS.set_num_threads(threads)
     t1 = time_ns()
@@ -111,8 +161,9 @@ for (mult, Nop) in matmul_functions
     
     Time = dt / Nop 
     GFLOPS = 1 / Time 
-    println( "GFLOPS = ", GFLOPS, " N =", N, "  threads =", threads )
-  end
+   # println( "GFLOPS = ", GFLOPS, " N =", N, "  threads =", threads, " time =", dt  )
+    println(  mult, " N =", N, " Nt =",  Nt," GFLOPS = ", GFLOPS )
+  #end
 
 end 
 
