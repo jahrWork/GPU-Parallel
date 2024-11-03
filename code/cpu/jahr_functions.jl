3cf49749d69cacc33816bcadd7fab7a09df05412
Pedro Rodríguez Jiménez <pedro.rjimenez@alumnos.upm.es>
2024-11-04 00:43:00 +0100

    Reorganize CPU folder, add @distributed parallelism and BLAS mul! function

--- code/cpu/jahr_functions.jl
+++ code/cpu/jahr_functions.jl
@@ -16,6 +16,8 @@ using LoopVectorization
 using Octavian
 using Tullio
 using Strided
+using Distributed
+
 
 function my_matrix_multiplication(A, B)
 
@@ -74,83 +76,61 @@ function my_efficient_matrix_multiplication2(A, B)
     return C
 end
 
-function mult_Nt_parallel_1_thread(A, B)
-
+function mult_Nt_parallel_1_thread___(A, B)
     (N, M) = size(A)
     (M, L) = size(B)
     C = zeros(Float32, (N, L))
-
-
     Threads.@threads for k in 1:Nt
         BLAS.set_num_threads(1)
         C = k * A * B
     end
-
     return C
 end
 
-function mult_No_parallel_1_thread(A, B)
-
+function mult_No_parallel_1_thread___(A, B)
     (N, M) = size(A)
     (M, L) = size(B)
     C = zeros(Float32, (N, L))
-
-
     BLAS.set_num_threads(1)
     for k in 1:Nt
         C = k * A * B
     end
-
     return C
 end
 
-function mult_Nt_times_parallel___(A, B)
-
+function mult_Nt_times_parallel______(A, B)
     (N, M) = size(A)
     (M, L) = size(B)
     C = zeros(Float32, (N, L))
-
-
     for k in 1:Nt
         C = k * A * B
     end
-
     return C
 end
 
-function matrix_mult______________(A, B)
-
-
+function matrix_mult_________________(A, B)
     return A * B
-
 end
 
-function matrix_mult_________alloc(A, B)
-
+function matrix_mult____________alloc(A, B)
     (N, M) = size(A)
     (M, L) = size(B)
     C = zeros(Float32, (N, L))
-
     C = A * B
-
     return C
 end
 
-function matrix_mult_________turbo(A, B)
-
+function matrix_mult____________turbo(A, B)
     @assert size(A, 2) == size(B, 1)
     m, n, p = size(A, 1), size(A, 2), size(B, 2)
-
     C = zeros(Float32, (m, p))
-
     @turbo for i = 1:m, j = 1:p, k = 1:n
         C[i, j] += A[i, k] * B[k, j]
     end
     return C
-
 end
 
-function matrix_mult_________tullio(A, B)
+function matrix_mult___________tullio(A, B)
     C = zeros(Float32, size(A, 1), size(B, 2))
     @tullio C[i, j] = A[i, k] * B[k, j]
     return C
@@ -161,12 +141,36 @@ function matrix_mult_________octavian(A, B)
     return C
 end
 
-function matrix_mult_________strided(A, B)
+function matrix_mult__________strided(A, B)
     C = zeros(Float32, size(A, 1), size(B, 2))
     @strided C .= A * B
     return C
 end
 
+function matrix_mult______distributed(A, B)
+    C = zeros(Float32, size(A, 1), size(B, 2))
+    @distributed for i in axes(A, 1)
+        for j in axes(B, 2)
+            C[i, j] = dot(A[i, :], B[:, j])
+        end
+    end
+    return C
+end
+
+function matrix_mult_distributed_sync(A, B)
+    C = zeros(Float32, size(A, 1), size(B, 2))
+    @sync @distributed for i in axes(A, 1)
+        for j in axes(B, 2)
+            C[i, j] = dot(A[i, :], B[:, j])
+        end
+    end
+    return C
+end
+
+function matrix__________________mul!(A, B)
+    C = mul!(similar(A, Float32, size(A, 1), size(B, 2)), A, B)
+    return C
+end
 
 
 
@@ -202,15 +206,18 @@ matmul_functions = (
     # (my_matrix_multiplication, 2*N^3), 
     # (my_efficient_matrix_multiplication, 2*N^3),
     # (my_efficient_matrix_multiplication2, 2*N^3),
-    (mult_No_parallel_1_thread, 2 * N^3 * Nt),
-    (mult_Nt_parallel_1_thread, 2 * N^3 * Nt),
-    (mult_Nt_times_parallel___, 2 * N^3 * Nt),
-    (matrix_mult_________alloc, 2 * N^3),
-    (matrix_mult______________, 2 * N^3),
-    (matrix_mult_________turbo, 2 * N^3),
-    (matrix_mult_________tullio, 2 * N^3),
-    (matrix_mult_________octavian, 2 * N^3),
-    (matrix_mult_________strided, 2 * N^3)
+    (mult_No_parallel_1_thread___, 2 * N^3 * Nt),
+    (mult_Nt_parallel_1_thread___, 2 * N^3 * Nt),
+    (mult_Nt_times_parallel______, 2 * N^3 * Nt),
+    (matrix_mult____________alloc, 2 * N^3),
+    (matrix_mult_________________, 2 * N^3),
+    (matrix_mult____________turbo, 2 * N^3),
+    (matrix_mult___________tullio, 2 * N^3),
+    # (matrix_mult_________octavian, 2 * N^3),
+    # (matrix_mult__________strided, 2 * N^3),
+    # (matrix_mult______distributed, 2 * N^3),
+    # (matrix_mult_distributed_sync, 2 * N^3),
+    (matrix__________________mul!, 2 * N^3)
 )
 
 
