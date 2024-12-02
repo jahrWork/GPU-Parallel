ce2f848a4540017db56814339043b1e068162467
Pedro Rodríguez Jiménez <pedro.rjimenez@alumnos.upm.es>
2024-12-02 12:58:47 +0100

    pretty print tables

--- new_branch/system_info.jl
+++ new_branch/system_info.jl
@@ -1,5 +1,7 @@
 using CUDA, Hwloc, CpuId
 
+# Remeber to enter your cpu_frequency manually
+
 # GPU Functions
 function get_GPU_architecture(capability)
     if capability.major == 9
@@ -67,6 +69,7 @@ end
 
 # CPU Functions
 function get_avx_value(string_cpuid)
+
     if occursin("256 bit", string_cpuid)
         return 8
     elseif occursin("512 bit", string_cpuid)
@@ -78,18 +81,18 @@ end
 
 function cpu_info(return_max_gflops=false)
     topology = Hwloc.topology_load()
-    num_cores = num_physical_cores()
-    num_threads = num_virtual_cores()
-    cpu_frequency = 4.5  # GHz (Adjust as needed)
+    N_cores = num_physical_cores()
+    N_threads = num_virtual_cores()
     cpuid = cpuinfo()
     string_cpuid = string(cpuid)
+    cpu_frequency = 4.5  # GHz (Adjust as needed)
     AVX_value = get_avx_value(string_cpuid)
 
     if return_max_gflops
         if AVX_value > 0
-            # Sigo pensado que esto es incorrecto, AVX_value en AVX2 es 8, pero sobre dos registros de 256 bits, fala otro factor de 2 por lo tanto.
+            # Sigo pensado que esto es incorrecto, AVX_value en AVX2 es 8, pero sobre dos registros de 256 bits, falta otro factor de 2 por lo tanto.
             # Se puede ver con nuestras pruebas (conseguimos casi 700GFLOPS en mi Ryzen 5 5600X con AVX2, marcando el teórico 432GFLOPS)
-            Theoretical_time = 1e9 / (cpu_frequency * 1e9 * AVX_value * 2 * num_cores) 
+            Theoretical_time = 1e9 / (cpu_frequency * 1e9 * AVX_value * 2 * N_cores) 
             return 1 / Theoretical_time
         else
             return 0
@@ -98,14 +101,14 @@ function cpu_info(return_max_gflops=false)
         println("--------------------------------------------------------")
         println("CPU Information")
         println(" ")
-        println("CPU Cores: ", num_cores)
-        println("CPU Threads: ", num_threads)
+        println("CPU Cores: ", N_cores)
+        println("CPU Threads: ", N_threads)
         println("CPU Frequency: ", cpu_frequency, " GHz           (GHz adjusted manually)")
         println("AVX support: ", occursin("256 bit", string_cpuid))
         println("AVX-512 support: ", occursin("512 bit", string_cpuid))
         println("AVX Value: ", AVX_value)
         if AVX_value > 0
-            Theoretical_time = 1e9 / (cpu_frequency * 1e9 * AVX_value * 2 * num_cores)
+            Theoretical_time = 1e9 / (cpu_frequency * 1e9 * AVX_value * 2 * N_cores)
             println("CPU Theoretical max GFLOPS: ", round(1 / Theoretical_time, digits=2), " GFLOPS")
         else
             println("AVX not supported. Cannot compute theoretical GFLOPS.")
@@ -122,4 +125,12 @@ end
 
 # Run the system_info function to get the CPU and GPU information
 # system_info()
+
+# Run the cpu_info function to get the CPU information
+# cpu_info()
+# cpu_info(true) # To get the CPU theoretical max GFLOPS
+
+# Run the gpu_info function to get the GPU information
+# gpu_info()
+# gpu_info(true) # To get the GPU theoretical max GFLOPS
 
