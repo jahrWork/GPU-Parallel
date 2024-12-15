c2646932e5565dc1ced2ef5b90e26f9389444d6b
Pedro Rodríguez Jiménez <pedro.rjimenez@alumnos.upm.es>
2024-12-15 15:40:03 +0100

    kernel_dot last week meeting

---
+++ 7. TFG/CUDA/kernel_dot_meeting_11dec.jl
@@ -0,0 +1,63 @@
+using CUDA
+CUDA.allowscalar(false)
+using BenchmarkTools
+
+N = 2050
+a = CUDA.rand(N)
+b = CUDA.rand(N)
+c = similar(a)
+
+typeof(a) <: AbstractArray
+
+function my_add!(c::AbstractArray, a::AbstractArray, b::AbstractArray)
+    for i in eachindex(c)
+        c[i] = a[i] + b[i]
+    end
+    nothing
+end
+
+function my_add_kernel!(c, a, b)
+    i = CUDA.threadIdx().x + (blockIdx().x - 1) * blockDim().x
+
+    # i = CUDA.threadIdx().x + (blockIdx().x - 1) * blockDim().x
+    #                        + (blockIdx().y - 1) * blockDim().x * blockDim().y
+
+
+    # Importante este if, porque si no, se intenta acceder a elementos de a y b que no existen.
+    # Ver vídeo, minuto 20:30. Intentaría coger elementos vacios del bloque que quede al final
+    # si no acaba justo completando el bloque.
+    if i <= length(c)
+        c[i] = a[i] + b[i]
+    end
+    return nothing
+end
+
+function my_add_custom!(c::CuArray, a::CuArray, b::CuArray)
+    @cuda blocks=cld(length(c), 1024) threads=1024 my_add_kernel!(c, a, b)
+end
+
+
+
+function my_dot_kernel!(dot, a, b)
+    dot = 0
+    i = CUDA.threadIdx().x + (blockIdx().x - 1) * blockDim().x
+    
+    # Importante este if, porque si no, se intenta acceder a elementos de a y b que no existen.
+    # Ver vídeo, minuto 20:30. Intentaría coger elementos vacios del bloque que quede al final
+    # si no acaba justo completando el bloque.
+    if i <= length(a)
+        dot += a[i] * b[i]
+    end
+    return nothing
+end
+
+function my_dot_custom!(c::CuArray, a::CuArray, b::CuArray)
+    @cuda blocks=cld(length(c), 1024) threads=1024 my_dot_kernel!(dot, a, b)
+end
+
+my_add_custom!(c, a, b)
+
+
+# Check if the addition is correct
+isapprox(Array(c), Array(a) .+ Array(b))
+
